5d5249f34d4c4ae543c13eab7562a2cb8265c176
827992983 <827992983@qq.com>
2018-12-10 22:28:08 +0800

    Add MfcHelloWorld

---
+++ DuiDemo/MfcHelloWorld/MfcHelloWorld.h
@@ -0,0 +1,32 @@
+
+// MfcHelloWorld.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CMfcHelloWorldApp: 
+// 有关此类的实现，请参阅 MfcHelloWorld.cpp
+//
+
+class CMfcHelloWorldApp : public CWinApp
+{
+public:
+	CMfcHelloWorldApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMfcHelloWorldApp theApp;
---
+++ DuiDemo/MfcHelloWorld/MfcHelloWorldDlg.h
@@ -0,0 +1,36 @@
+
+// MfcHelloWorldDlg.h : 头文件
+//
+
+#pragma once
+
+
+// CMfcHelloWorldDlg 对话框
+class CMfcHelloWorldDlg : public CDialogEx
+{
+// 构造
+public:
+	CMfcHelloWorldDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCHELLOWORLD_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton1();
+};
