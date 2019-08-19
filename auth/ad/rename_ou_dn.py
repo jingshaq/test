1626a4c2b36acfd6a983a7fa9366bd5a9ef2f7a2
827992983 <827992983@qq.com>
2019-08-19 19:40:18 +0800

    auth/

---
+++ auth/ad/rename_ou_dn.py
@@ -0,0 +1,21 @@
+import ldap
+import ldap.modlist as modlist
+
+server  = '10.11.12.9:389'
+user_dn = 'Administrator@myadserver.com'
+user_pw = 'Zhu88jie'
+
+fs_dn = 'ou=test,ou=temp0,dc=myadserver,dc=com'
+
+l = ldap.initialize("ldap://"+server)
+ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
+l.protocol_version = 3
+l.set_option(ldap.OPT_REFERRALS, 0)
+l.bind_s(user_dn, user_pw)
+
+new_dn = 'OU=temp0,DC=myadserver,DC=com'
+
+
+print(l.rename_s(fs_dn, "ou=new", new_dn))
+
+l.unbind_s()
