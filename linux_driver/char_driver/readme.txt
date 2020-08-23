42f8c30e1ebd4fde5388685464a6fb15c43e12fd
827992983 <827992983@qq.com>
2020-08-23 20:56:27 +0800

    finish test.c

--- linux_driver/char_driver/readme.txt
+++ linux_driver/char_driver/readme.txt
@@ -1,3 +1,5 @@
+说明：
+    本例中，驱动没有回归缓冲区位置，因此在test.c中在write数据后，调用lseek将文件指针修改到起始位置。
 1.执行make
 2.insmod chardriverdemo.ko
 3.mknode /dev/chardriverdemo c 211 0
--- linux_driver/char_driver/test.c
+++ linux_driver/char_driver/test.c
@@ -1,21 +1,38 @@
 #include<sys/types.h>
 #include<stdio.h>
+#include<string.h>
 #include<unistd.h>
 #include<fcntl.h>
 
 int main()
 {
-	char buf[]="Hello!";
+	int ret = 0, i = 0;
+	char write_buf[]="Hello!";
+	char read_buf[10] = {0};
 
 	int dev_fd=open("/dev/chardriverdemo",O_RDWR | O_NONBLOCK);
 	if(dev_fd < 0){
 		printf("open error!");
 		return 0;
 	}
-	if(!write(dev_fd,buf,6))
-		printf("write error");
-	if(!write(dev_fd,buf,6))
-		printf("write error");
+	
+	for(i=0; i<10; i++){
+		if(!write(dev_fd,write_buf,6))
+			printf("write error");
+	}
+
+	lseek(dev_fd, 0, SEEK_SET); // driver中没有修改offset, 因此这里必须重新定位到文件开始
+
+	while(1){
+		memset(read_buf, 0, 10);
+		ret = read(dev_fd, read_buf, 9);
+		printf("ret = %d\n", ret);
+		if(ret <= 0)
+			break;
+		else
+			printf("read: %s\n", read_buf);
+	}
+	
 
 	return 0;
 
