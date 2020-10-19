957d826151ead7290a751b70772e64b8b2994274
827992983 <827992983@qq.com>
2020-10-19 17:20:25 +0800

    add windows system get/set system sound volume

---
+++ WindowsSystemVolume/WindowsSystemVolume/WindowsSystemVolume.cpp
@@ -0,0 +1,106 @@
+// WindowsSystemVolume.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <windows.h> 
+#include <mmdeviceapi.h> 
+#include <endpointvolume.h>
+#include <audioclient.h>
+
+bool SetVolum(int volume)
+{
+	bool ret = false;
+	HRESULT hr;
+	IMMDeviceEnumerator* pDeviceEnumerator = 0;
+	IMMDevice* pDevice = 0;
+	IAudioEndpointVolume* pAudioEndpointVolume = 0;
+	IAudioClient* pAudioClient = 0;
+
+	try {
+		hr = CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL, CLSCTX_ALL, __uuidof(IMMDeviceEnumerator), (void**)&pDeviceEnumerator);
+		if (FAILED(hr)) throw "CoCreateInstance";
+		hr = pDeviceEnumerator->GetDefaultAudioEndpoint(eRender, eMultimedia, &pDevice);
+		if (FAILED(hr)) throw "GetDefaultAudioEndpoint";
+		hr = pDevice->Activate(__uuidof(IAudioEndpointVolume), CLSCTX_ALL, NULL, (void**)&pAudioEndpointVolume);
+		if (FAILED(hr)) throw "pDevice->Active";
+		hr = pDevice->Activate(__uuidof(IAudioClient), CLSCTX_ALL, NULL, (void**)&pAudioClient);
+		if (FAILED(hr)) throw "pDevice->Active";
+
+		float fVolume;
+		fVolume = volume / 100.0f;
+		hr = pAudioEndpointVolume->SetMasterVolumeLevelScalar(fVolume, &GUID_NULL);
+		if (FAILED(hr)) throw "SetMasterVolumeLevelScalar";
+
+		pAudioClient->Release();
+		pAudioEndpointVolume->Release();
+		pDevice->Release();
+		pDeviceEnumerator->Release();
+
+		ret = true;
+	}
+	catch (...) {
+		if (pAudioClient) pAudioClient->Release();
+		if (pAudioEndpointVolume) pAudioEndpointVolume->Release();
+		if (pDevice) pDevice->Release();
+		if (pDeviceEnumerator) pDeviceEnumerator->Release();
+		throw;
+	}
+
+	return ret;
+}
+
+int GetVolume()
+{
+	int volumeValue = 0;
+	HRESULT hr;
+	IMMDeviceEnumerator* pDeviceEnumerator = 0;
+	IMMDevice* pDevice = 0;
+	IAudioEndpointVolume* pAudioEndpointVolume = 0;
+	IAudioClient* pAudioClient = 0;
+
+	try {
+		hr = CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL, CLSCTX_ALL, __uuidof(IMMDeviceEnumerator), (void**)&pDeviceEnumerator);
+		if (FAILED(hr)) throw "CoCreateInstance";
+		hr = pDeviceEnumerator->GetDefaultAudioEndpoint(eRender, eMultimedia, &pDevice);
+		if (FAILED(hr)) throw "GetDefaultAudioEndpoint";
+		hr = pDevice->Activate(__uuidof(IAudioEndpointVolume), CLSCTX_ALL, NULL, (void**)&pAudioEndpointVolume);
+		if (FAILED(hr)) throw "pDevice->Active";
+		hr = pDevice->Activate(__uuidof(IAudioClient), CLSCTX_ALL, NULL, (void**)&pAudioClient);
+		if (FAILED(hr)) throw "pDevice->Active";
+
+		float fVolume;
+		hr = pAudioEndpointVolume->GetMasterVolumeLevelScalar(&fVolume);
+		if (FAILED(hr)) throw "SetMasterVolumeLevelScalar";
+
+		pAudioClient->Release();
+		pAudioEndpointVolume->Release();
+		pDevice->Release();
+		pDeviceEnumerator->Release();
+
+		volumeValue = fVolume * 100;
+	}
+	catch (...) {
+		if (pAudioClient) pAudioClient->Release();
+		if (pAudioEndpointVolume) pAudioEndpointVolume->Release();
+		if (pDevice) pDevice->Release();
+		if (pDeviceEnumerator) pDeviceEnumerator->Release();
+		throw;
+	}
+
+	return volumeValue;
+}
+
+
+int main()
+{
+	CoInitialize(0);
+
+	int volumeValue = GetVolume();
+	printf("当前音量为:%d.\r\n", volumeValue);
+	SetVolum(50);
+
+	CoUninitialize();
+	getchar();
+    return 0;
+}
+
